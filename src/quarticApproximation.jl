aa54ff14c01a723006a0cd0e51666f4181f87583
matthiashimmelmann <matthias.himmelmann@outlook.de>
2025-02-07 04:58:31 +0100

    up

--- src/quarticApproximation.jl
+++ src/quarticApproximation.jl
@@ -23,7 +23,7 @@ open("dataCSVdeltan4d10qn4d20complexPointsZ1Z2Z3.txt") do f
     end
 end
 include("LearnVanishingIdeal.jl")
-result, sampserror = LearnVanishingIdeal.approximateVanishingIdeal(totalarray, [4], quick=false, affine=false)
+result, sampserror = LearnVanishingIdeal.approximateVanishingIdeal(totalarray, [4]xys, quick=false, affine=false)
 @var x,y,z
 result = [[round(entry,digits=3) for entry in value]'*LearnVanishingIdeal.projVeronese(4,[x,y,z]) for value in result][1]
 
@@ -33,7 +33,7 @@ a₀ = randn(ComplexF64, 3)
 F = System(G; variables=[x,y,z], parameters=a)
 start = solve(F; target_parameters=a₀)
 start_sols = solutions(start)
-k = 10^4
+k = 10^5
 
 function map_step(R)
     R = R ./ norm(R)
@@ -55,6 +55,6 @@ sample_matrix = hcat(points...)
 orig_sample_matrix = hcat(orig_points...)
 
 plt = scatter(sample_matrix[1,:], sample_matrix[2,:],
-        legend = false, grid = false, framestyle = :origin, markersize=1, markercolor = :grey, ylims = (-3,3), xlims = (-3,3), size=(800,800))
+        legend = false, grid = false, framestyle = :origin, markersize=1, markercolor = :grey, ylims = (-5,5), xlims = (-5,5), size=(800,800))
 scatter!(plt, orig_sample_matrix[1,:], orig_sample_matrix[2,:],
-        markersize=3, markercolor = :steelblue)
+        markersize=5, markercolor = :steelblue)
